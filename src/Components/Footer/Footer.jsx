9b65c571ec4b74396f6a8c7126f557136ced2156
Jocke <joakim-johansson99@hotmail.com>
2025-03-12 13:43:24 +0100

    gh

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <hr/>
+        <p>© 2024 Joakim Johansson</p>
+        </div>
+  )
+}
+
+export default Footer
