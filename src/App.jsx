3dce684b979e3fe27a50f7879880117e7a3c1ef4
Jocke <joakim-johansson99@hotmail.com>
2025-03-12 13:35:18 +0100

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import NavBar from './Components/NavBar/NavBar'
 import Projects from './Components/Projects/Projects'
-
+import Footer from './Components/Footer/Footer'
 export const App = () => {
   return (
     <div>
