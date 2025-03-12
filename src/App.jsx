541ee31c087bc0e6dbd90e8be18794b43c0d4823
Jocke <joakim-johansson99@hotmail.com>
2025-03-12 13:30:32 +0100

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/footer'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import NavBar from './Components/NavBar/NavBar'
 import Projects from './Components/Projects/Projects'
